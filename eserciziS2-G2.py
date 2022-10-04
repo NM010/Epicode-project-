cfd921911c56cf2928059d5d74349f55ffc6fc20
Mario Ninotta <114725561+NM010@users.noreply.github.com>
2022-10-04 18:33:09 +0200

    Add files via upload

---
+++ eserciziS2-G2.py
@@ -0,0 +1,39 @@
+"""
+l_serbatoio = float(input("inserire litri nel serbatoio:"))
+e_benzina = float(input("inserire efficienza km/l: "))
+p_benzina = float(input("inserire prezzo per litro: "))
+
+c_benzina = (100 / e_benzina ) * p_benzina
+d_benzina = e_benzina * l_serbatoio
+print("costo per 100 km: " , c_benzina)
+print("puoi percorrere ancora: " , d_benzina)
+
+"""
+
+"""
+
+metri = float(input("inserisci misura in metri: "))
+
+piede = metri * 3.281
+pollice = metri * 39.37
+miglia = metri / 1609
+
+
+
+print(piede, "piedi")
+print(pollice, "pollici")
+print(miglia, "miglia")
+
+"""
+
+
+"""
+string = input("scrivi una frase: ")
+first_3 = string[0:3]
+last_3 = string[-3:]
+puntini = " ... "
+
+concatena = first_3 + puntini + last_3
+print(concatena)
+"""
+
