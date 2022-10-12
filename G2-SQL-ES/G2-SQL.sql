7f489f712756d7c63421da7f06baac1654012f76
Mario Ninotta <114725561+NM010@users.noreply.github.com>
2022-10-12 09:03:29 +0200

    Delete G2-SQL.sql

--- G2-SQL-ES/G2-SQL.sql
+++
@@ -1,7 +0,0 @@
-SELECT NomeCantante
-FROM CANTANTE
-JOIN ESECUZIONE ON  CANTANTE.CodiceReg=ESECUZIONE.CodiceReg                    
-JOIN AUTORE ON ESECUZIONE.TitoloCanz=AUTORE.TitoloCanzone    
-WHERE Nome=NomeCantante AND Nome LIKE 'D%'; 
-
-
